a813005ec26c939f86a90397ecf486fc8e7b9726
Javier <javieralburges@gmail.com>
2024-12-20 07:14:16 -0400

    se agrego el openGraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,20 @@ export const metadata: Metadata = {
   title: "GearSoftCA",
   verification: {google: "sIyRrMDGwLjnrlANbzwYf1tXWVcVNSjL_yf76grlDBo"},
   description: "Esta es una empresa de servicio digitales profesionales de diferentes áreas relacionada con las tecnologías de la información",
+  openGraph: {
+    title: "GearSoftCA",
+    type: "website",
+    url: "https://gearsoftca.vercel.app/",
+    images: [
+      {
+        url: "https://www.gearsoftca.com/images/gearsoft-logo.png",
+        width: 800,
+        height: 600,
+        alt: "GearSoftCA Logo",
+      },
+    ],
+    description: "Esta es una empresa de servicio digitales profesionales de diferentes áreas relacionada con las tecnologías de la información",
+  },
 };
 
 export default function RootLayout({children,}: {children: React.ReactNode}) {
