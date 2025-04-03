4b6e82ebd30a8a0d4a25a1aee13aa9c263d07c2d
Javier <javieralburges@gmail.com>
2025-04-02 21:27:39 -0400

    Ya listo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://gearsoftca.vercel.app', // URL base de tu sitio
-    generateRobotsTxt: true, // Generar robots.txt
-    sitemapSize: 100000, // Asegurar que todas las rutas quepan en un solo archivo
-    changefreq: 'daily', // Frecuencia de cambio sugerida
-    priority: 0.7, // Prioridad por defecto para las URLs
+    siteUrl: 'https://gearsoftca.vercel.app',
+    generateRobotsTxt: true,
 };
