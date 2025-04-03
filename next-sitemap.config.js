5f9fe9e1a5e957b01f8e78a830f368ae9ff697dd
Javier <javieralburges@gmail.com>
2025-04-02 21:12:17 -0400

    mini cambio

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://gearsoftca.vercel.app',
-    generateRobotsTxt: true,
+    siteUrl: 'https://gearsoftca.vercel.app', // URL base de tu sitio
+    generateRobotsTxt: true, // Generar robots.txt
+    sitemapSize: 5000, // Tamaño máximo de cada archivo sitemap
+    changefreq: 'daily', // Frecuencia de cambio sugerida
+    priority: 0.7, // Prioridad por defecto para las URLs
 };
